3b8cdc715cdbaa611049dca858dd1860dd72ff18
Mitch <mitch-roblox@users.noreply.github.com>
2018-02-01 06:17:11 -0800

    Update dependent cookbooks ssl_certificate to ~> 2.1 and consul to ~> 3.1 (#5)

--- metadata.rb
+++ metadata.rb
@@ -10,5 +10,5 @@ supports 'centos', '>= 6.4'
 supports 'redhat', '>= 6.4'
 supports 'ubuntu', '>= 12.04'
 
-depends 'ssl_certificate', '~> 1.11'
-depends 'consul', '~> 2.1'
+depends 'ssl_certificate', '~> 2.1'
+depends 'consul', '~> 3.1'
